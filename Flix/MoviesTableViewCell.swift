9af2fc0be0fa02d3aea54d6853cf1fe2c7614903
Simona Virga <simonavirga@Simonas-MacBook-Pro.local>
2018-01-14 21:25:10 -0500

    Here is my commit message

---
+++ Flix/MoviesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesTableViewCell.swift
+//  Flix
+//
+//  Created by Simona Virga on 1/10/18.
+//  Copyright © 2018 Simona Virga. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
