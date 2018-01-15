63c3c06f76d7f458304cad8c343b56254ba90360
svirga <svirga@purdue.edu>
2018-01-14 21:50:26 -0500

    Add files via upload

--- Flix/MoviesTableViewCell.swift
+++ Flix/MoviesTableViewCell.swift
@@ -8,14 +8,21 @@
 
 import UIKit
 
-class MoviesTableViewCell: UITableViewCell {
+class MoviesTableViewCell: UITableViewCell
+{
 
-    override func awakeFromNib() {
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieDescriptionLabel: UILabel!
+    
+    override func awakeFromNib()
+    {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
+    override func setSelected(_ selected: Bool, animated: Bool)
+    {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
